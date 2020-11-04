1953404c00cc4027a3f588e6b6e1ccfd6d37ea3c
selby <fetz.selby@gmail.com>
2020-11-05 00:33:42 +0100

    alpha push

---
+++ app.js
@@ -0,0 +1,73 @@
+const io = require("socket.io-client");
+const feathers = require("@feathersjs/client");
+
+// Set the year and region id, region id 1 is Ashanti Region
+const [year, regionId] = ['2016', '1'];
+
+// Host address
+const host = 'http://127.0.0.1:5002';
+
+// Socket and channel setup
+const socket = io(host);
+const service = feathers();
+
+// Request functions :)
+
+getConstituencies = async() => 
+    await service.service("constituencies").find({ query: { year, regionId } });
+
+getParties = async() => 
+    await service.service("parties").find();
+
+getCandidates = async() =>
+    await service.service("candidates").find({ query: { year, regionId } });
+
+getRegions = async() =>
+    await service.service("regions").find();
+
+getNationalAnalysis = async() =>
+    await service.service("national_analysis").find({ query: { year } });
+
+getRegionalAnalysis = async() =>
+    await service.service("regional_analysis").find({ query: { year, regionId } });
+
+getSeat = async() =>
+    await service.service("constituency_seats").find({ query: { year, regionId } });
+
+
+async function create() {
+    
+    service.configure(feathers.socketio(socket));
+    service.configure(feathers.authentication());
+
+    // Get all constituencies for 2016 in a region
+    const constituencies = await getConstituencies();
+
+    // Get all parties
+    const parties = await getParties();
+
+    // Get all candidates of 2016 in a region
+    const candidates = await getCandidates();
+
+    // Get all regions
+    const regions = await getRegions();
+
+    // Get regional analysis of a year
+    const regionalAnalysis = await getRegionalAnalysis();
+
+    // Get national analysis of a year
+    const nationalAnalysis = await getNationalAnalysis();
+
+    // Get get seats won
+    const seatsWon = await getSeat();
+
+    console.log('constituencies, ', constituencies);
+    // console.log('parties, ', parties);
+    // console.log('candidates, ', candidates);
+    // console.log('regions, ', regions);
+    // console.log('regionalAnalysis, ', regionalAnalysis);
+    // console.log('nationalAnalysis, ', nationalAnalysis);
+    // console.log('seats, ', seatsWon);
+}
+
+create();
